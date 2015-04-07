c641091aa64547fa43751430ab0a659d77195f9f
Navjeet Dhaliwal <nsd@ualberta.ca>
2015-04-07 12:57:57 -0600

    menu working

--- project2.py
+++ project2.py
@@ -1,5 +1,5 @@
 import bsddb3 as bsddb
-import random
+import random, os, sys, time
 
 #create this directory in tmp before running this program
 DA_FILE = "tmp/nsd_db/my_db"
@@ -8,7 +8,6 @@ DA_SIZE = 10000
 SEED = 10000000
 
 def main():
-    def main_menu():
     os.system('clear')
     
     print ("Welcome!")
@@ -24,56 +23,61 @@ def main():
  
     return
  
+def Create():
+    #Where database is created and populated
+    print("HERE DATABASE IS CREATED AND POPULATED")
+    time.sleep(2)
+    back()
+    
+def Key():
+    #Search with given key
+    print("HERE PROGRAM SEARCHES WITH GIVEN KEY")
+    time.sleep(2)
+    back()
+    
+def Data():
+    #search with given data
+    print("HERE PROGRAM SEARCHES WITH GIVEN DATA")
+    time.sleep(2)
+    back()
+
+def Range():
+    #Search with range of keys
+    print("HERE PROGRAM SEARCHES A GIVEN RANGE OF KEYS")
+    time.sleep(2)
+    back()
+ 
+def Destroy():
+    #Destroy the database
+    print("HERE WE DESTROY THE DATABASE")
+    time.sleep(2)
+    back()
+ 
+# Back to main menu
+def back():
+    main()
+ 
+# Exit program
+def exit():   
+    sys.exit()
+
 # Execute menu
 def exec_menu(choice):
     ch = choice.lower()
     if ch == '':
-        menu_actions['main_menu']()
+        menu_actions['main']()
     else:
         try:
             menu_actions[ch]()
         except KeyError:
             print ("Invalid selection, please try again.\n")
             time.sleep(1)
-            menu_actions['main_menu']()
+            menu_actions['main']()
     return
- 
-def Create():
-    #Where database is created and populated
-    print("HERE DATABASE IS CREATED AND POPULATED")
-    back()
-    
-def Key():
-    #Search with given key
-    print("HERE PROGRAM SEARCHES WITH GIVEN KEY")
-    back()
-    
-def Data():
-    #search with given data
-    print("HERE PROGRAM SEARCHES WITH GIVEN DATA")
-    back()
-
-def Range():
-    #Search with range of keys
-    print("HERE PROGRAM SEARCHES A GIVEN RANGE OF KEYS")
-    back()
-
-def Destroy():
-    #Destroy the database
-    print("HERE WE DESTROY THE DATABASE")
-    back()
- 
-# Back to main menu
-def back():
-    main_menu()
- 
-# Exit program
-def exit():   
-    sys.exit()
  
 # Menu definition
 menu_actions = {
-    'main_menu': main_menu,
+    'main_menu': main,
     '1': Create,
     '2': Key,
     '3': Data,
@@ -82,3 +86,5 @@ menu_actions = {
     '0': exit,
 }
 
+if __name__ == "__main__":
+    main()
