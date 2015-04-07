b344004cb36a85537a31243bf992c3f99d215b49
Navjeet Dhaliwal <nsd@ualberta.ca>
2015-04-07 12:26:32 -0600

    added the file for the main program

---
+++ project2.py
@@ -0,0 +1,11 @@
+import bsddb3 as bsddb
+import random
+
+#create this directory in tmp before running this program
+DA_FILE = "tmp/nsd_db/my_db"
+#ten thousand for now but needs to be one hundred thousand to hand in
+DA_SIZE = 10000
+SEED = 10000000
+
+def main():
+    
