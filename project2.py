878ec9ceb92efffac3f19f89af4ceaf721fcadff
Navjeet Dhaliwal <nsd@ualberta.ca>
2015-04-08 12:20:25 -0600

    creating an destroying database

--- project2.py
+++ project2.py
@@ -1,64 +1,100 @@
 import bsddb3 as bsddb
-import random, os, sys, time
+import random, os, sys, time 
 
 #create this directory in tmp before running this program
-DA_FILE = "tmp/nsd_db/my_db"
+DA_FILE = "/tmp/nsd_db/my_db"
 #ten thousand for now but needs to be one hundred thousand to hand in
-DA_SIZE = 10000
+DB_SIZE = 10000
 SEED = 10000000
+db = 0;
+def get_random():
+    return random.randint(0, 63)
+def get_random_char():
+    return chr(97 + random.randint(0, 25))
+
 
 def main():
-    os.system('clear')
-    
-    print ("Welcome!")
-    print ("Please choose the menu you want to start:")
-    print ("1. Create and populate a database")
-    print ("2. Retrieve records with a given key")
-    print ("3. Retrieve records with a given data")
-    print ("4. Retrieve records with a given range key values")
-    print ("5. Destroy the database")
-    print ("\n0. Exit")
-    choice = input(" >>  ")
-    exec_menu(choice)
+    while True:
+        os.system('clear')
+        
+        print ("Welcome!")
+        print ("Please choose the menu you want to start:")
+        print ("1. Create and populate a database")
+        print ("2. Retrieve records with a given key")
+        print ("3. Retrieve records with a given data")
+        print ("4. Retrieve records with a given range key values")
+        print ("5. Destroy the database")
+        print ("\n0. Exit")
+        choice = input(" >>  ")
+        exec_menu(choice)
  
-    return
  
 def Create():
     #Where database is created and populated
     print("HERE DATABASE IS CREATED AND POPULATED")
-    time.sleep(2)
-    back()
+    time.sleep(1)
+    try:
+        db = bsddb.btopen(DA_FILE, "w")
+    except:
+        print("DB doesn't exist, creating a new one")
+        db = bsddb.btopen(DA_FILE, "c")
+    random.seed(SEED)
+
+    for index in range(DB_SIZE):
+        krng = 64 + get_random()
+        key = ""
+        for i in range(krng):
+            key += str(get_random_char())
+        vrng = 64 + get_random()
+        value = ""
+        for i in range(vrng):
+            value += str(get_random_char())
+        print (key)
+        print (value)
+        print ("")
+        key = key.encode(encoding='UTF-8')
+        value = value.encode(encoding='UTF-8')
+        db[key] = value
+    try:
+        db.close()
+    except Exception as e:
+        print (e)
+
+
+
     
 def Key():
     #Search with given key
     print("HERE PROGRAM SEARCHES WITH GIVEN KEY")
     time.sleep(2)
-    back()
     
 def Data():
     #search with given data
     print("HERE PROGRAM SEARCHES WITH GIVEN DATA")
     time.sleep(2)
-    back()
 
 def Range():
     #Search with range of keys
     print("HERE PROGRAM SEARCHES A GIVEN RANGE OF KEYS")
     time.sleep(2)
-    back()
  
 def Destroy():
     #Destroy the database
     print("HERE WE DESTROY THE DATABASE")
     time.sleep(2)
-    back()
+    try:
+        db.close()
+        bsddb.remove("/tmp/nsd_db/my_db")
+    except:
+        print("could not remove")
  
 # Back to main menu
 def back():
     main()
  
 # Exit program
-def exit():   
+def exitProgram():
+    Destroy()
     sys.exit()
 
 # Execute menu
@@ -83,7 +119,7 @@ menu_actions = {
     '3': Data,
     '4': Range,
     '5': Destroy,
-    '0': exit,
+    '0': exitProgram,
 }
 
 if __name__ == "__main__":
